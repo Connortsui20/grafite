ef8b240fd73b0c7f977cfc21027488b658eb138b
Connor Tsui <cjtsui@amazon.com>
2024-07-19 22:36:24 +0000

    first working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-//! TODO need to be able to take control of the Elias-Fano encoding parameters.
+//! TODO docs.
 
+use rand::prelude::*;
 use std::ops::Range;
 use vers_vecs::EliasFanoVec;
 
@@ -16,10 +17,48 @@ pub struct GrafiteHasher {
 }
 
 /// TODO docs.
+fn is_prime(n: u64) -> bool {
+    match n {
+        0 | 1 => false,
+        2 => true,
+        _ if n % 2 == 0 => false,
+        _ => !(1..)
+            .map(|x| 2 * x + 1)
+            .take_while(|&x| x * x <= n)
+            .any(|factor| n % factor == 0),
+    }
+}
+
+/// Generates a random number until it generates a prime, and then returns that prime number.
+fn gen_prime() -> u64 {
+    let mut rng = rand::thread_rng();
+
+    loop {
+        let attempt: u32 = rng.gen();
+        dbg!(attempt);
+
+        if is_prime(attempt as u64) {
+            println!("Found prime {}", attempt);
+            return attempt as u64;
+        }
+    }
+}
+
+/// TODO docs.
 impl GrafiteHasher {
     /// Creates a new hash function. TODO more docs.
-    pub fn new(num_elements: u64, max_interval: u64, epsilon: f64) -> Self {
-        let upper = num_elements
+    pub fn new(num_elements: usize, max_interval: u64, epsilon: f64) -> Self {
+        assert!(
+            epsilon < 0.99,
+            "Why are you trying to create a false positive rate of above 0.99?"
+        );
+        assert!(epsilon > 0.0, "epsilon must be greater than 0.0");
+        assert!(
+            max_interval <= Self::max_range_interval(num_elements, epsilon),
+            "The input maximum interval is impossible given the other parameters"
+        );
+
+        let upper = (num_elements as u64)
             .checked_mul(max_interval)
             .expect("We should not have any overflow on calculating the reduced universe size.");
 
@@ -29,12 +68,21 @@ impl GrafiteHasher {
             .checked_mul(lower)
             .expect("We should not have any overflow on calculating the reduced universe size.");
 
+        dbg!(upper, lower, reduced_universe_size);
+
+        let mut rng = rand::thread_rng();
+
         Self {
-            c1: 0,
-            c2: 0,
-            p: 3,
+            c1: rng.gen(),
+            c2: rng.gen(),
+            p: gen_prime(), // TODO
             r: reduced_universe_size,
         }
+    }
+
+    /// TODO docs.
+    fn max_range_interval(num_elements: usize, epsilon: f64) -> u64 {
+        ((u64::MAX as f64) * epsilon) as u64 / num_elements as u64
     }
 
     // A hash function taken from a pairwise-independent family.
@@ -63,13 +111,18 @@ pub struct RangeFilter {
 /// The `RangeFilter` must be built on items that are able to be turned into a 64-bit integer.
 impl RangeFilter {
     /// Creates a new `RangeFilter` given a slice of values.
-    pub fn new(values: &[u64], hasher: GrafiteHasher) -> Self {
-        // Hash the input set.
-        let mut hashes: Vec<u64> = values.iter().cloned().map(|x| hasher.hash(x)).collect();
+    pub fn new<I>(values: I, hasher: GrafiteHasher) -> Self
+    where
+        I: Iterator<Item = u64>,
+    {
+        // Hash all items in the input set.
+        let mut hashes: Vec<u64> = values.map(|x| hasher.hash(x)).collect();
 
         // Sort and then remove all duplicates.
         hashes.sort_unstable();
         hashes.dedup();
+
+        assert!(hashes[hashes.len() - 1] < hasher.r);
 
         Self {
             ef: EliasFanoVec::from_slice(&hashes),
@@ -107,5 +160,38 @@ impl RangeFilter {
             // between start and end.
             Some(predecessor) => predecessor >= start_hash,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_prime() {
+        let primes = [2, 3, 5, 7, 11, 13, 17, 19];
+
+        assert!(primes.iter().copied().all(is_prime));
+    }
+
+    #[test]
+    fn test_basic() {
+        let values = [1, 2, 3, 7, 8, 9, 15, 20];
+
+        let hasher = GrafiteHasher::new(values.len(), 20, 0.01);
+
+        let rf = RangeFilter::new(values.iter().copied(), hasher);
+
+        assert!(rf.query(0..20));
+        assert!(rf.query(0..10));
+        assert!(rf.query(0..5));
+        assert!(rf.query(3..5));
+        assert!(!rf.query(4..5));
+        assert!(!rf.query(4..6));
+        assert!(rf.query(4..7));
+        assert!(rf.query(4..8));
+        assert!(rf.query(4..10));
+        assert!(!rf.query(10..14));
+        assert!(rf.query(10..15));
     }
 }
